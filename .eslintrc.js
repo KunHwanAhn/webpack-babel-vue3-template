a8fe3d5008ac6f042e03adf07930b7761dfd7723
Conan Kunhwan Ahn <kunhwon@gmail.com>
2021-01-10 00:57:31 +0900

    [npm] ESLint 설정 추가 

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
