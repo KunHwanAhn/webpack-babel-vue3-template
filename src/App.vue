bc6c09aa9191d15a57f4b6c1145b7ce11d72d2e3
Conan Kunhwan Ahn <kunhwon@gmail.com>
2021-01-10 15:56:18 +0900

    [npm] Vue 의존성과 웹팩 설정 추가

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sample">
+    Vue app
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+.sample {
+  color: tomato;
+}
+</style>
