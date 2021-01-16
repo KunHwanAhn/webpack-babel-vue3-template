9b04604be18cf7ca6b0d2145a933a29f1dc78f9e
Conan Kunhwan Ahn <kunhwon@gmail.com>
2021-01-16 17:28:01 +0900

    [eslint] ES Lint 규칙 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,24 @@ module.exports = {
     },
   },
   rules: {
-    'no-console': 'error',
-    'no-debugger': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e', // for e.returnvalue
+      ],
+    }],
+    'vue/no-boolean-default': 'error',
   },
 };
